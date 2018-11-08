b327bc11135f387f7d935815ddc8bde1a6bf5915
SailHe <396103442@qq.com>
2018-11-08 17:03:16 +0800

    dba password

---
+++ src/com/nit/dbms/introduction/ProjectConstant.java
@@ -0,0 +1,12 @@
+package com.nit.dbms.introduction;
+
+/**
+ * Description: 无描述类<p>
+ *
+ * @Package: com.nit.dbms.introduction
+ * @author: SailHe
+ * @date: 2018/11/8 15:53
+ */
+public class ProjectConstant {
+    public static final String dbaPass  = "输入你自己的密码";
+}
