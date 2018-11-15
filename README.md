30aaca67d0148aba57fd260de43cf38f467f9509
SailHe <396103442@qq.com>
2018-11-15 16:00:03 +0800

    整理 + update readme.md

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Java Demo
-基于java的示例
-1. DBA 数据库
+基于java
+1. DBA 数据库: 各种连接池的Demo位于: com.nit.dbms.action.util
 2. 文件
 3. GUI
-4. 来源: https://gitee.com/nit-zju_admin/dba/tree/master
-5. 目前并未上传远程仓库
+4. fork来源: https://gitee.com/nit-zju_admin/dba/tree/master
+5. 请自行使用根目录下的sql文件构建MySQL数据库
+6. mybatis + hibernate
+
--- src/com/nit/dbms/jdbc/JDBCTester.java
+++ src/com/nit/dbms/jdbc/JDBCTester.java
@@ -1,15 +1,14 @@
 package com.nit.dbms.jdbc;
 
-import com.nit.dbms.introduction.StudentBean;
 import com.nit.dbms.introduction.StudentDao;
 
 public class JDBCTester {
     public static void main(String[] args) {
         StudentDao studentDao = new StudentDao();
-        //studentDao.getAll();
-        for (int i = 0; i < 10; i++) {
+        studentDao.getAll();
+        /*for (int i = 0; i < 10; i++) {
             studentDao.insert(new StudentBean("01700500" + i, "01", "张三丰", "男", "1997-10-11 00:00:00", "660780"));
-        }
+        }*/
         //studentDao.getAll();
         //studentDao.update(new StudentBean("", "", "张三丰", "", "", "666666"));
         //studentDao.getAll();
